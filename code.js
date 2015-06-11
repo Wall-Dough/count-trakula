45fc203b88ceb87a30edcc84bbdc31bf5f20e38e
Andrew <awalden0808@gmail.com>
2015-06-11 18:17:16 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,181 @@
+var counters = [{"name":"abcdefg","id":0,"value":0,"slug":"test1"},{"name":"Foo Bar","id":1,"value":0,"slug":"test2"},{"name":"Change CSS","id":2,"value":0,"slug":"test3"},{"name":"Short Round","id":3,"value":0,"slug":"test4"},{"name":"Staples","id":4,"value":0,"slug":"test5"},{"name":"camelCase","id":5,"value":0,"slug":"test6"},{"name":"ogubmetqne","id":6,"value":0,"slug":"test7"},{"name":"joicigphql","id":7,"value":0,"slug":"test8"},{"name":"ibnbxtanrs","id":8,"value":0,"slug":"test9"},{"name":"lcpgaajwse","id":9,"value":0,"slug":"test10"},{"name":"ugsqyinfxf","id":10,"value":0,"slug":"test11"},{"name":"gunihqqtio","id":11,"value":0,"slug":"test12"},{"name":"vaervfjvsn","id":12,"value":0,"slug":"test13"},{"name":"okwbckxhwm","id":13,"value":0,"slug":"test14"},{"name":"vognrfppyx","id":14,"value":0,"slug":"test15"},{"name":"lkgodvmnru","id":15,"value":0,"slug":"test16"},{"name":"uutjrytzmm","id":16,"value":0,"slug":"test17"},{"name":"vsihszdfjn","id":17,"value":0,"slug":"test18"},{"name":"cdegqxmxcd","id":18,"value":0,"slug":"test19"},{"name":"bmpaclsxfd","id":19,"value":0,"slug":"test20"},{"name":"mzzxkyadno","id":20,"value":0,"slug":"test21"},{"name":"sgmprtznnl","id":21,"value":0,"slug":"test22"},{"name":"sazjnchfuv","id":22,"value":0,"slug":"test23"},{"name":"esnlpibmbe","id":23,"value":0,"slug":"test24"},{"name":"xyulpubtuq","id":24,"value":0,"slug":"test25"},{"name":"catkdfriuf","id":25,"value":0,"slug":"test26"},{"name":"aaxcwmuvqv","id":26,"value":0,"slug":"test27"},{"name":"ldguxlqbha","id":27,"value":0,"slug":"test28"},{"name":"gftxyrhcdw","id":28,"value":0,"slug":"test29"},{"name":"tbnrnyramq","id":29,"value":0,"slug":"test30"},{"name":"lhtrtvsuej","id":30,"value":0,"slug":"test31"},{"name":"qptrhtlxaq","id":31,"value":0,"slug":"test32"},{"name":"ymkbpytrqz","id":32,"value":0,"slug":"test33"},{"name":"sgbrqwxoic","id":33,"value":0,"slug":"test34"},{"name":"onheebovsl","id":34,"value":0,"slug":"test35"},{"name":"circagppws","id":35,"value":0,"slug":"test36"},{"name":"ykkcqldbth","id":36,"value":0,"slug":"test37"},{"name":"msbrcatsul","id":37,"value":0,"slug":"test38"},{"name":"hrlpvsbxbv","id":38,"value":0,"slug":"test39"},{"name":"bkudxrgpyo","id":39,"value":0,"slug":"test40"},{"name":"jqrjfwpckc","id":40,"value":0,"slug":"test41"},{"name":"ymsmesnzdl","id":41,"value":0,"slug":"test42"},{"name":"ptohwpyrde","id":42,"value":0,"slug":"test43"},{"name":"kjuakiuhaa","id":43,"value":0,"slug":"test44"},{"name":"yrpxtsbtbv","id":44,"value":0,"slug":"test45"},{"name":"ucenswmeaf","id":45,"value":0,"slug":"test46"},{"name":"csvllwbtsm","id":46,"value":0,"slug":"test47"},{"name":"mmrqjyzviw","id":47,"value":0,"slug":"test48"},{"name":"zphvdsxmfb","id":48,"value":0,"slug":"test49"},{"name":"ueipojdfzg","id":49,"value":0,"slug":"test50"},{"name":"qurwkeybxd","id":50,"value":0,"slug":"test51"},{"name":"cmasuxxtgh","id":51,"value":0,"slug":"test52"},{"name":"rriqkdxkxf","id":52,"value":0,"slug":"test53"},{"name":"pdjqkteavd","id":53,"value":0,"slug":"test54"},{"name":"vkahrmuirz","id":54,"value":0,"slug":"test55"},{"name":"zaddlanvxk","id":55,"value":0,"slug":"test56"},{"name":"vhwrexfhws","id":56,"value":0,"slug":"test57"},{"name":"evvscwelwb","id":57,"value":0,"slug":"test58"},{"name":"xxbgoiamij","id":58,"value":0,"slug":"test59"},{"name":"cvbztydvhk","id":59,"value":0,"slug":"test60"},{"name":"uzhfxsgbcn","id":60,"value":0,"slug":"test61"},{"name":"wtoutwdqfz","id":61,"value":0,"slug":"test62"},{"name":"hnolhunxtx","id":62,"value":0,"slug":"test63"},{"name":"bkprzzacpa","id":63,"value":0,"slug":"test64"},{"name":"ryfxivsnml","id":64,"value":0,"slug":"test65"},{"name":"fxmajnaqkj","id":65,"value":0,"slug":"test66"},{"name":"verxviprla","id":66,"value":0,"slug":"test67"},{"name":"klqtrepxmu","id":67,"value":0,"slug":"test68"},{"name":"bufwvkqiis","id":68,"value":0,"slug":"test69"},{"name":"vvxgpembvw","id":69,"value":0,"slug":"test70"},{"name":"sbaxheojfq","id":70,"value":0,"slug":"test71"},{"name":"kdevlbkcis","id":71,"value":0,"slug":"test72"},{"name":"dgqoegamzk","id":72,"value":0,"slug":"test73"},{"name":"vwnmvwumnk","id":73,"value":0,"slug":"test74"},{"name":"frhryuyxtr","id":74,"value":0,"slug":"test75"},{"name":"szybogiiny","id":75,"value":0,"slug":"test76"},{"name":"kizcdsawnm","id":76,"value":0,"slug":"test77"},{"name":"gyaqznxwfx","id":77,"value":0,"slug":"test78"},{"name":"vpinptkjqf","id":78,"value":0,"slug":"test79"},{"name":"vhoyyxqerh","id":79,"value":0,"slug":"test80"},{"name":"imfxcizous","id":80,"value":0,"slug":"test81"},{"name":"jjwarnrjbi","id":81,"value":0,"slug":"test82"},{"name":"pkdgodenje","id":82,"value":0,"slug":"test83"},{"name":"rgpoktktre","id":83,"value":0,"slug":"test84"},{"name":"ghbkfzqaut","id":84,"value":0,"slug":"test85"},{"name":"knmccjhyyt","id":85,"value":0,"slug":"test86"},{"name":"etlljifvpn","id":86,"value":0,"slug":"test87"},{"name":"mtbrrcyqpz","id":87,"value":0,"slug":"test88"},{"name":"qnathbamxl","id":88,"value":0,"slug":"test89"},{"name":"pjydnpcpza","id":89,"value":0,"slug":"test90"},{"name":"krnvqgsgoo","id":90,"value":0,"slug":"test91"},{"name":"xtpatdmvbl","id":91,"value":0,"slug":"test92"},{"name":"xznvzymzwl","id":92,"value":0,"slug":"test93"},{"name":"jxtsgguzsh","id":93,"value":0,"slug":"test94"},{"name":"lvhjgiasih","id":94,"value":0,"slug":"test95"},{"name":"ywdaphorrs","id":95,"value":0,"slug":"test96"},{"name":"rzxwkxrblq","id":96,"value":0,"slug":"test97"},{"name":"zjxlgveeib","id":97,"value":0,"slug":"test98"},{"name":"untcldmgta","id":98,"value":0,"slug":"test99"},{"name":"bpcmxojfnk","id":99,"value":0,"slug":"test100"},{"name":"afaujeshbv","id":100,"value":0,"slug":"test101"},{"name":"erbqeimvju","id":101,"value":0,"slug":"test102"},{"name":"ynpqpjujis","id":102,"value":0,"slug":"test103"},{"name":"tlufpujzhw","id":103,"value":0,"slug":"test104"},{"name":"gaxptmljdq","id":104,"value":0,"slug":"test105"},{"name":"bnnufctrxe","id":105,"value":0,"slug":"test106"}];
+
+var lookup;
+
+function createLookup(toIndex) {
+	var lookup = {};
+	for (var i = 0; i < toIndex.length; i++) {
+		lookup[toIndex[i].slug] = toIndex[i];
+	}
+	return lookup;
+}
+
+function addToTrie(counter, trie, position) {
+	if (position >= counter.name.length) {
+		return;
+	}
+	var character = counter.name.charAt(position).toLowerCase();
+	if (trie[character] == null) {
+		trie[character] = {};
+	}
+	if (trie[character]["counters"] == null) {
+		trie[character]["counters"] = [];
+	}
+	trie[character]["counters"].push(counter);
+	addToTrie(counter, trie[character], position + 1);
+}
+
+function createTrie(toIndex) {
+	var trie = {};
+	for (var i = 0; i < toIndex.length; i++) {
+		addToTrie(toIndex[i], trie, 0);
+	}
+	return trie;
+}
+
+function incrementListener(e) {
+	var link = e.currentTarget;
+	var elementId = link.getAttribute("id");
+	var counterSlug = elementId.substring(0, elementId.lastIndexOf("-up-link"));
+	var counter = lookup[counterSlug];
+	counter.value++;
+	document.getElementById(counterSlug + "-value").innerHTML = counter.value.toString();
+}
+
+function incrementListener2(e) {
+	var link = e.currentTarget;
+	var elementId = link.getAttribute("id");
+	var counterSlug = elementId.substring(0, elementId.lastIndexOf("-add-link"));
+	var counter = lookup[counterSlug];
+	counter.value++;
+	document.getElementById(counterSlug + "-value").innerHTML = counter.value.toString();
+}
+
+function decrementListener(e) {
+	var link = e.currentTarget;
+	var elementId = link.getAttribute("id");
+	var counterSlug = elementId.substring(0, elementId.lastIndexOf("-down-link"));
+	var counter = lookup[counterSlug];
+	counter.value--;
+	document.getElementById(counterSlug + "-value").innerHTML = counter.value.toString();
+}
+
+function decrementListener2(e) {
+	var link = e.currentTarget;
+	var elementId = link.getAttribute("id");
+	var counterSlug = elementId.substring(0, elementId.lastIndexOf("-subtract-link"));
+	var counter = lookup[counterSlug];
+	counter.value--;
+	document.getElementById(counterSlug + "-value").innerHTML = counter.value.toString();
+}
+
+function setValue(e) {
+	var button = e.currentTarget;
+	var elementId = button.getAttribute("id");
+	var slug = elementId.substring(0, elementId.lastIndexOf("-value-submit"));
+	var textInput = document.getElementById(slug + "-value-input");
+	var value = textInput.value;
+	textInput.value = "";
+	var counter = lookup[slug];
+	counter.value = parseInt(value);
+	document.getElementById(slug + "-value").innerHTML = counter.value.toString();
+}
+
+function getFilteredCounters(searchTerm, position, trie) {
+	if (position >= searchTerm.length) {
+		return trie["counters"];
+	}
+	var character = searchTerm.charAt(position).toLowerCase();
+	if (trie[character] == null) {
+		return [];
+	}
+	else {
+		return getFilteredCounters(searchTerm, position + 1, trie[searchTerm.charAt(position)]);
+	}
+}
+
+function updateFilter() {
+	var searchTerm = document.getElementById("search").value;
+	if (searchTerm == "") {
+		loadCounters(counters);
+		return;
+	}
+	var filtered = [];
+	filtered = getFilteredCounters(searchTerm, 0, trie);
+	loadCounters(filtered);
+}
+
+function loadCounters(toLoad) {
+	var counterTable = document.getElementById("counter-table");
+	counterTable.innerHTML = "";
+	for (var i = 0; i < toLoad.length; i++) {
+		// Initialize counter
+		var counterRow = document.createElement("tr");
+		counterRow.setAttribute("id", toLoad[i].slug + "-row");
+		counterRow.setAttribute("class", "counter-row");
+		// Arrows
+		var counterArrows = document.createElement("td");
+		counterArrows.setAttribute("id", toLoad[i].slug + "-arrows");
+		counterArrows.setAttribute("class", "counter-arrows");
+		// Up arrow
+		var upLink = document.createElement("a");
+		upLink.setAttribute("id", toLoad[i].slug + "-up-link");
+		upLink.setAttribute("class", "counter-arrow-link");
+		upLink.addEventListener("click", incrementListener);
+		var upImg = document.createElement("img");
+		upImg.setAttribute("src", "images/up-arrow.png");
+		upImg.setAttribute("id", toLoad[i].slug + "-up-img");
+		upImg.setAttribute("class", toLoad[i].slug + "-arrow-img");
+		upLink.appendChild(upImg);
+		counterArrows.appendChild(upLink);
+		// Down arrow
+		var downLink = document.createElement("a");
+		downLink.setAttribute("id", toLoad[i].slug + "-down-link");
+		downLink.setAttribute("class", "counter-arrow-link");
+		downLink.addEventListener("click", decrementListener);
+		var downImg = document.createElement("img");
+		downImg.setAttribute("src", "images/down-arrow.png");
+		downImg.setAttribute("id", toLoad[i].slug + "-down-img");
+		downImg.setAttribute("class", toLoad[i].slug + "-arrow-img");
+		downLink.appendChild(downImg);
+		counterArrows.appendChild(downLink);
+		counterRow.appendChild(counterArrows);
+		// Counter value element
+		var counterValue = document.createElement("td");
+		counterValue.setAttribute("class", "counter-value");
+		counterValue.setAttribute("id", toLoad[i].slug + "-value");
+		counterValue.innerHTML = toLoad[i].value.toString();
+		counterRow.appendChild(counterValue);
+		// Counter name element
+		var counterName = document.createElement("td");
+		counterName.setAttribute("class", "counter-name");
+		counterName.setAttribute("id", toLoad[i].slug + "-name");
+		counterName.innerHTML = toLoad[i].name;
+		counterRow.appendChild(counterName);
+		// Value input element
+		var valueSetCell = document.createElement("td");
+		valueSetCell.setAttribute("class", "value-set");
+		valueSetCell.setAttribute("id", toLoad[i].slug + "-value-set");
+		var valueSetInput = document.createElement("input");
+		valueSetInput.setAttribute("class", "value-input");
+		valueSetInput.setAttribute("id", toLoad[i].slug + "-value-input");
+		valueSetInput.setAttribute("type", "text");
+		valueSetCell.appendChild(valueSetInput);
+		// Value submit element
+		var valueSetSubmit = document.createElement("input");
+		valueSetSubmit.setAttribute("class", "value-submit");
+		valueSetSubmit.setAttribute("id", toLoad[i].slug + "-value-submit");
+		valueSetSubmit.setAttribute("type", "button");
+		valueSetSubmit.setAttribute("value", "Set Value");
+		valueSetSubmit.addEventListener("click", setValue);
+		valueSetCell.appendChild(valueSetSubmit);
+		counterRow.appendChild(valueSetCell);
+		counterTable.appendChild(counterRow);
+	}
+}
+
+window.onload = function () {
+	lookup = createLookup(counters);
+	trie = createTrie(counters);
+	loadCounters(counters);
+}
